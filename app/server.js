242b431ac2c909abddcb5d9161f9e42c3f9af514
Indy-GA <1380029@cegepmontpetit.ca>
2026-04-24 18:14:54 -0400

    test 6

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Hello de DEV Toad.   r",
+  dev: "Hello de DEV Toad. Test reussi",
   qa: "Hello de l'environnement de QA",
   prod: "Hello de l'environnement de PROD",
 };
